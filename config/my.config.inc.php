9ef9453348f52d3bb36b08860ac0b201be836e46
Nils Gummerlich <nils@gummerlich.de>
2022-09-04 14:21:03 +0200

    main

---
+++ config/my.config.inc.php
@@ -0,0 +1,11 @@
+<?php
+$config = array (
+  'dev' => true,
+  'background_image' => 'url(../img/bg.jpg)',
+  'background_admin' => 'url(../img/bg.jpg)',
+  'background_chroma' => 'url(../img/bg.jpg)',
+  'print' => 
+  array (
+    'cmd' => 'mspaint /pt ',
+  ),
+);
