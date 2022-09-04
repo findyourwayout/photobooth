3a0db107378ffdfe0a1428704932ee138729898f
Nils Gummerlich <nils@gummerlich.de>
2022-09-04 14:00:48 +0200

    ca

---
+++ config/my.config.inc.php
@@ -0,0 +1,15 @@
+<?php
+$config = array (
+  'dev' => true,
+  'previewFromCam' => true,
+  'cups_button' => true,
+  'background_image' => 'url(../img/bg.jpg)',
+  'background_admin' => 'url(../img/bg.jpg)',
+  'background_chroma' => 'url(../img/bg.jpg)',
+  'rounded_corners' => true,
+  'language' => 'de',
+  'print' => 
+  array (
+    'cmd' => 'mspaint /pt ',
+  ),
+);
